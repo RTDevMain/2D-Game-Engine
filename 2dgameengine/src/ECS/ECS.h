553971cf9e6f4fc50e5d311babd936ae1d591e58
RTDevMain <ryky94@gmail.com>
2024-12-03 13:30:48 -0800

    Updated Registry

--- 2dgameengine/src/ECS/ECS.h
+++ 2dgameengine/src/ECS/ECS.h
@@ -2,6 +2,7 @@
 #define ECS_H
 
 #include <bitset>
+#include <memory>
 #include <typeindex>
 #include <unordered_map>
 #include <utility>
@@ -165,10 +166,10 @@ class Registry {
 		Registry() = default;
 
 		// TODO:
+		void Update();
 
 		Entity CreateEntity();
 
-		void Update();
 
 		void AddEntityToSystem();
 
@@ -178,14 +179,43 @@ class Registry {
 
 		template <typename T> bool HasComponent(Entity entity);
 
-		//
-		// KillEntity()
-		// AddSystem()
-		// RemoveSystem()
-		// HasSystem()
-		// GetSystem()
+		
+		
+		template <typename TSystem, typename ...TArgs> void AddSystem(TArgs&& ...args);
+		template <typename TSystem> void RemoveSystem();
+		template <typename TSystem> bool HasSystem() const;
+		template <typename TSystem> TSystem& GetSystem() const;
+
+		// Checks the component signature of an entity and adds it to systems that are interested
+		void AddEntityToSystems(Entity entity);
 		
 };
+
+
+template <typename TSystem, typename ...TArgs>
+void Registry::AddSystem(TArgs&& ...args) {
+	TSystem* newSystem(new TSystem(std::forward<TArgs>(args)...));
+	systems.insert(std::make_pair(std::type_index(typeid(TSystem)), newSystem));
+}
+
+template <typename TSystem>
+void Registry::RemoveSystem(){
+	auto system = systems.find(std::type_index(typeid(TSystem)));
+	systems.erase(system);
+}
+
+template <typename TSystem>
+bool Registry::HasSystem() const {
+	return systems.find(std::type_index(typeid(TSystem)))!= systems.end();
+}
+
+template <typename TSystem>
+TSystem& Registry::GetSystem() const {
+	auto system = systems.find(std::type_index(typeid(TSystem)));
+
+	// Don't use '.' when working with pointer use -> instead //
+	return *(std::static_pointer_cast<TSystem>(system->second));
+}
 
 
 template <typename TComponent> 
@@ -193,6 +223,8 @@ void System::RequireComponent() {
 	const auto componentId = Component<TComponent>::GetId();
 	componentSignature.set(componentId);
 }
+
+
 
 template <typename TComponent, typename ...TArgs>
 void Registry::AddComponent(Entity entity, TArgs&& ...args) {
